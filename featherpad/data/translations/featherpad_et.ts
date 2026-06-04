6a4a65fb94ff54eaeaa14a7674c314d9c8475612
Weblate <noreply@weblate.org>
2026-06-04 22:29:03 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/featherpad/et/
    Translation: Tsujan/FeatherPad

--- featherpad/data/translations/featherpad_et.ts
+++ featherpad/data/translations/featherpad_et.ts
@@ -957,7 +957,7 @@
     <message>
         <location filename="../../fpwin.cpp" line="1989"/>
         <source>Cannot start process!</source>
-        <translation type="unfinished"></translation>
+        <translation>Protsessi käivitamine ei õnnestu!</translation>
     </message>
     <message>
         <location filename="../../fpwin.cpp" line="2758"/>
